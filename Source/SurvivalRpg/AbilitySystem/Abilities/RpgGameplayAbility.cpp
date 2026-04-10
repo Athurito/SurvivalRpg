59220d539c7faad1e465d1443ba08e8f82ffaf3d
SteveBob <88212395+Athurito@users.noreply.github.com>
2026-04-10 20:19:47 +0200

    Add ability activation policies and automatic activation on spawn

--- Source/SurvivalRpg/AbilitySystem/Abilities/RpgGameplayAbility.cpp
+++ Source/SurvivalRpg/AbilitySystem/Abilities/RpgGameplayAbility.cpp
@@ -3,6 +3,7 @@
 
 #include "RpgGameplayAbility.h"
 
+#include "AbilitySystemComponent.h"
 #include "SurvivalRpg/Core/Player/RpgPlayerController.h"
 
 
@@ -16,6 +17,39 @@ URpgGameplayAbility::URpgGameplayAbility(const FObjectInitializer& ObjectInitial
 ARpgPlayerController* URpgGameplayAbility::GetRpgPlayerControllerFromActorInfo() const
 {
 	return (CurrentActorInfo ? Cast<ARpgPlayerController>(CurrentActorInfo->PlayerController.Get()) : nullptr);
+}
+
+void URpgGameplayAbility::TryActivateAbilityOnSpawn(const FGameplayAbilityActorInfo* ActorInfo,
+	const FGameplayAbilitySpec& Spec) const
+{
+	// Try to activate if activation policy is on spawn.
+	if (ActorInfo && !Spec.IsActive() && (ActivationPolicy == ERpgAbilityActivationPolicy::OnSpawn))
+	{
+		UAbilitySystemComponent* ASC = ActorInfo->AbilitySystemComponent.Get();
+		const AActor* AvatarActor = ActorInfo->AvatarActor.Get();
+
+		// If avatar actor is torn off or about to die, don't try to activate until we get the new one.
+		if (ASC && AvatarActor && !AvatarActor->GetTearOff() && (AvatarActor->GetLifeSpan() <= 0.0f))
+		{
+			const bool bIsLocalExecution = (NetExecutionPolicy == EGameplayAbilityNetExecutionPolicy::LocalPredicted) || (NetExecutionPolicy == EGameplayAbilityNetExecutionPolicy::LocalOnly);
+			const bool bIsServerExecution = (NetExecutionPolicy == EGameplayAbilityNetExecutionPolicy::ServerOnly) || (NetExecutionPolicy == EGameplayAbilityNetExecutionPolicy::ServerInitiated);
+
+			const bool bClientShouldActivate = ActorInfo->IsLocallyControlled() && bIsLocalExecution;
+			const bool bServerShouldActivate = ActorInfo->IsNetAuthority() && bIsServerExecution;
+
+			if (bClientShouldActivate || bServerShouldActivate)
+			{
+				ASC->TryActivateAbility(Spec.Handle);
+			}
+		}
+	}
+}
+
+void URpgGameplayAbility::OnGiveAbility(const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilitySpec& Spec)
+{
+	Super::OnGiveAbility(ActorInfo, Spec);
+	K2_OnAbilityAdded();
+	TryActivateAbilityOnSpawn(ActorInfo, Spec);
 }
 
 bool URpgGameplayAbility::HasPlayerController() const
--- Source/SurvivalRpg/AbilitySystem/Abilities/RpgGameplayAbility.h
+++ Source/SurvivalRpg/AbilitySystem/Abilities/RpgGameplayAbility.h
@@ -7,6 +7,49 @@
 #include "RpgGameplayAbility.generated.h"
 
 class ARpgPlayerController;
+
+
+
+/**
+ * ERpgAbilityActivationPolicy
+ *
+ *	Defines how an ability is meant to activate.
+ */
+UENUM(BlueprintType)
+enum class ERpgAbilityActivationPolicy : uint8
+{
+	// Try to activate the ability when the input is triggered.
+	OnInputTriggered,
+
+	// Continually try to activate the ability while the input is active.
+	WhileInputActive,
+
+	// Try to activate the ability when an avatar is assigned.
+	OnSpawn
+};
+
+
+/**
+ * ERpgAbilityActivationGroup
+ *
+ *	Defines how an ability activates in relation to other abilities.
+ */
+UENUM(BlueprintType)
+enum class ERpgAbilityActivationGroup : uint8
+{
+	// Ability runs independently of all other abilities.
+	Independent,
+
+	// Ability is canceled and replaced by other exclusive abilities.
+	Exclusive_Replaceable,
+
+	// Ability blocks all other exclusive abilities from activating.
+	Exclusive_Blocking,
+
+	MAX	UMETA(Hidden)
+};
+
+
 /**
  * 
  */
@@ -21,6 +64,16 @@ public:
 	UFUNCTION(BlueprintCallable, Category = "Rpg|Ability")
 	ARpgPlayerController* GetRpgPlayerControllerFromActorInfo() const;
 	
+	ERpgAbilityActivationPolicy GetActivationPolicy() const { return ActivationPolicy; }
+	ERpgAbilityActivationGroup GetActivationGroup() const { return ActivationGroup; }
+
+	void TryActivateAbilityOnSpawn(const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilitySpec& Spec) const;
+	
+	virtual void OnGiveAbility(const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilitySpec& Spec) override;
+
+	
+public:
+	
 	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "UI")
 	bool bShouldShowInAbilitiesBar = false;
 	
@@ -30,4 +83,21 @@ public:
 protected:
 	UFUNCTION(BlueprintCallable, Category = "AbilitySystem")
 	bool HasPlayerController() const;
+	
+	/** Called when this ability is granted to the ability system component. */
+	UFUNCTION(BlueprintImplementableEvent, Category = Ability, DisplayName = "OnAbilityAdded")
+	void K2_OnAbilityAdded();
+	
+	/** Called when this ability is removed from the ability system component. */
+	UFUNCTION(BlueprintImplementableEvent, Category = Ability, DisplayName = "OnAbilityRemoved")
+	void K2_OnAbilityRemoved();
+	
+protected:
+	// Defines how this ability is meant to activate.
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Rpg|Ability Activation")
+	ERpgAbilityActivationPolicy ActivationPolicy;
+
+	// Defines the relationship between this ability activating and other abilities activating.
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Rpg|Ability Activation")
+	ERpgAbilityActivationGroup ActivationGroup;
 };
