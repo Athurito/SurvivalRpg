c5bf556791212b964eee419b8ec4b23a17507eef
SteveBob <88212395+Athurito@users.noreply.github.com>
2026-05-20 22:23:15 +0200

    Remove deprecated `RpgGameplayAbility_Collect` and `RpgVitalsViewModelComponent`; add new `RpgHUD` and `RpgActivatableWidget` UI systems.

--- Source/SurvivalRpg/SurvivalRpg.Build.cs
+++ Source/SurvivalRpg/SurvivalRpg.Build.cs
@@ -24,6 +24,7 @@ public class SurvivalRpg : ModuleRules
 			"NetCore",
 			"StateTreeModule",
 			"UMG",
+			"CommonUI",
 			"GameplayAbilities", "GameplayTags", "GameplayTasks",
 		});
 
---
+++ Source/SurvivalRpg/UI/RpgHUD.cpp
@@ -0,0 +1,60 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "RpgHUD.h"
+
+#include "AbilitySystemComponent.h"
+#include "AbilitySystemGlobals.h"
+#include "Components/GameFrameworkComponentManager.h"
+#include "UObject/UObjectIterator.h"
+
+ARpgHUD::ARpgHUD(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
+{
+	PrimaryActorTick.bStartWithTickEnabled = false;
+}
+
+void ARpgHUD::PreInitializeComponents()
+{
+	Super::PreInitializeComponents();
+	UGameFrameworkComponentManager::AddGameFrameworkComponentReceiver(this);
+}
+
+void ARpgHUD::BeginPlay()
+{
+	UGameFrameworkComponentManager::SendGameFrameworkComponentExtensionEvent(this, UGameFrameworkComponentManager::NAME_GameActorReady);
+	Super::BeginPlay();
+}
+
+void ARpgHUD::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	UGameFrameworkComponentManager::RemoveGameFrameworkComponentReceiver(this);
+	Super::EndPlay(EndPlayReason);
+}
+
+void ARpgHUD::GetDebugActorList(TArray<AActor*>& InOutList)
+{
+	UWorld* World = GetWorld();
+
+	Super::GetDebugActorList(InOutList);
+
+	for (TObjectIterator<UAbilitySystemComponent> It; It; ++It)
+	{
+		if (UAbilitySystemComponent* ASC = *It)
+		{
+			if (!ASC->HasAnyFlags(RF_ClassDefaultObject | RF_ArchetypeObject))
+			{
+				AActor* AvatarActor = ASC->GetAvatarActor();
+				AActor* OwnerActor = ASC->GetOwnerActor();
+
+				if (AvatarActor && UAbilitySystemGlobals::GetAbilitySystemComponentFromActor(AvatarActor))
+				{
+					AddActorToDebugList(AvatarActor, InOutList, World);
+				}
+				else if (OwnerActor && UAbilitySystemGlobals::GetAbilitySystemComponentFromActor(OwnerActor))
+				{
+					AddActorToDebugList(OwnerActor, InOutList, World);
+				}
+			}
+		}
+	}
+}
